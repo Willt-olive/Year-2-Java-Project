6297da882212fa4e81a497d32a78ead38715209e
William <williamdmoore2002@gmail.com>
2023-04-06 18:31:28 +0100

    Button Groups
    added groups to the buttons so that when one is selected the other turns off.

--- naivebayes/src/Assignment/naiveInterface.java
+++ naivebayes/src/Assignment/naiveInterface.java
@@ -10,14 +10,20 @@ public class naiveInterface  extends JFrame implements ActionListener{
 	
 	JFrame frame;
 	JButton Button1;
+	JButton Button2;
+	ButtonGroup G1;
 	JRadioButton jRadioButton1;
 	JRadioButton jRadioButton2;
+	ButtonGroup G2;
 	JRadioButton jRadioButton3;
 	JRadioButton jRadioButton4;
+	ButtonGroup G3;
 	JRadioButton jRadioButton5;
 	JRadioButton jRadioButton6;
+	ButtonGroup G4;
 	JRadioButton jRadioButton7;
 	JRadioButton jRadioButton8;
+	ButtonGroup G5;
 	JRadioButton jRadioButton9;
 	JRadioButton jRadioButton10;
 	JLabel label1;
@@ -26,7 +32,6 @@ public class naiveInterface  extends JFrame implements ActionListener{
 	JLabel label4;
 	JLabel label5;
 	JOptionPane newWindow;
-	JTextArea textArea;
 	
 	
 	public naiveInterface(String title) {
@@ -40,11 +45,16 @@ public class naiveInterface  extends JFrame implements ActionListener{
 	    
 	    /////////////////////////////////	    
 	    
-	    Button1= new JButton();
+	    Button1 = new JButton();
 	    Button1.setText("Enter");
 	    Button1.setToolTipText("Enter Data");
 	    
+	    Button2 = new JButton();
+	    Button2.setText("Test");
+	    Button2.setToolTipText("Test Data");
+	    
 	    Button1.addActionListener(this);
+	    Button2.addActionListener(this);
 	    
 	    //////////////////////////////////////////
 	    
@@ -102,26 +112,41 @@ public class naiveInterface  extends JFrame implements ActionListener{
 		
 		//////////////////////////////////////////
 	    
-	    textArea = new JTextArea();
+		G1 = new ButtonGroup();
+		G2 = new ButtonGroup();
+		G3 = new ButtonGroup();
+		G4 = new ButtonGroup();
+		G5 = new ButtonGroup();
 	    
 	    //////////////////////////////////////////
 	    
 	    frame.add(label1);
 	    frame.add(jRadioButton1);
 	    frame.add(jRadioButton2);
+	    G1.add(jRadioButton1);
+		G1.add(jRadioButton2);
 	    frame.add(label2);
 	    frame.add(jRadioButton3);
 	    frame.add(jRadioButton4);
+	    G2.add(jRadioButton3);
+		G2.add(jRadioButton4);
 	    frame.add(label3);
 	    frame.add(jRadioButton5);
 	    frame.add(jRadioButton6);
+	    G3.add(jRadioButton5);
+		G3.add(jRadioButton6);
 	    frame.add(label4);
 	    frame.add(jRadioButton7);
 	    frame.add(jRadioButton8);
+	    G4.add(jRadioButton7);
+		G4.add(jRadioButton8);
 	    frame.add(label5);
 	    frame.add(jRadioButton9);
 	    frame.add(jRadioButton10);
+	    G5.add(jRadioButton9);
+		G5.add(jRadioButton10);
 	    frame.add(Button1);
+	    frame.add(Button2);
 	}
 
 	@Override
@@ -129,26 +154,11 @@ public class naiveInterface  extends JFrame implements ActionListener{
 		
 		if(e.getSource() == Button1)
 		{
-			String name = enterName.getText();
-			String surname = enterSurname.getText();
-			String city = enterCity.getText();
-			
-			
-			if (name.equals("") || surname.equals("") || city.equals(""))
-			{
-				JOptionPane.showMessageDialog(this, "Please Fill in Text Fields");
-			}
-			else
-			{
-				enterName.setText(null);
-				enterSurname.setText(null);
-				enterCity.setText(null);
-			}
+			JOptionPane.showMessageDialog(this, "Please Fill in Text Fields");
 		}
 		else if(e.getSource()== Button2)
 		{
-			frame.add(textArea);
-			textArea.setColumns(6);
+			JOptionPane.showMessageDialog(this, "Please Fill in Text Fields");
 		}
 	}
 }
