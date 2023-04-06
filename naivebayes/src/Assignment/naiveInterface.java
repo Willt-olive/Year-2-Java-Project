b196b99028e386a8f94415137c2653bff1f1d928
William <williamdmoore2002@gmail.com>
2023-04-06 17:19:47 +0100

    Interface Change
    interface change so that they are radio buttons with all the options
    no functionality

--- naivebayes/src/Assignment/naiveInterface.java
+++ naivebayes/src/Assignment/naiveInterface.java
@@ -9,13 +9,23 @@ import javax.swing.*;
 public class naiveInterface  extends JFrame implements ActionListener{
 	
 	JFrame frame;
-	JButton Save;
-	JButton showAll;
-	JButton delete;
+	JButton Button1;
+	JRadioButton jRadioButton1;
+	JRadioButton jRadioButton2;
+	JRadioButton jRadioButton3;
+	JRadioButton jRadioButton4;
+	JRadioButton jRadioButton5;
+	JRadioButton jRadioButton6;
+	JRadioButton jRadioButton7;
+	JRadioButton jRadioButton8;
+	JRadioButton jRadioButton9;
+	JRadioButton jRadioButton10;
+	JLabel label1;
+	JLabel label2;
+	JLabel label3;
+	JLabel label4;
+	JLabel label5;
 	JOptionPane newWindow;
-	JTextField enterName;
-	JTextField enterSurname;
-	JTextField enterCity;
 	JTextArea textArea;
 	
 	
@@ -24,28 +34,17 @@ public class naiveInterface  extends JFrame implements ActionListener{
 		
 		frame = new JFrame(title);
 		frame.setVisible(true);
-	    frame.setSize(450, 300);
+	    frame.setSize(290, 300);
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    frame.setLayout(new FlowLayout());
 	    
 	    /////////////////////////////////	    
 	    
-	    Save= new JButton();
-	    Save.setText("Save");
-	    Save.setToolTipText("This Button Adds Text Field To Array");
+	    Button1= new JButton();
+	    Button1.setText("Enter");
+	    Button1.setToolTipText("Enter Data");
 	    
-	    showAll = new JButton();
-	    showAll.setText("Show All");
-	    showAll.setToolTipText("This Button Show The Array");
-	    
-	    delete = new JButton();
-	    delete.setText("delete");
-	    delete.setToolTipText("This Button Delets The Whole Array");
-	    
-	    
-	    Save.addActionListener(this);
-	    showAll.addActionListener(this);
-	    delete.addActionListener(this);
+	    Button1.addActionListener(this);
 	    
 	    //////////////////////////////////////////
 	    
@@ -53,36 +52,82 @@ public class naiveInterface  extends JFrame implements ActionListener{
 	    
 	    //////////////////////////////////////////
 	    
-	    enterName = new JTextField();
-	    enterName.setColumns(6);
-	    enterName.setToolTipText("Enter your first name here");
-	    
-	    enterSurname = new JTextField();
-	    enterSurname.setColumns(6);
-	    enterSurname.setToolTipText("Enter your surname here");
-	    
-	    enterCity = new JTextField();
-	    enterCity.setColumns(6);
-	    enterCity.setToolTipText("Enter your city here");
+	    jRadioButton1 = new JRadioButton();
+		jRadioButton1.setText("Male");
+		
+		jRadioButton2 = new JRadioButton();
+		jRadioButton2.setText("Female");
+		
+		jRadioButton3 = new JRadioButton();
+		jRadioButton3.setText("Yes");
+		
+		jRadioButton4 = new JRadioButton();
+		jRadioButton4.setText("No");
+	
+		jRadioButton5 = new JRadioButton();
+		jRadioButton5.setText("Yes");
+		
+		jRadioButton6 = new JRadioButton();
+		jRadioButton6.setText("No");
+		
+		jRadioButton7 = new JRadioButton();
+		jRadioButton7.setText("Rural");
+	
+		jRadioButton8 = new JRadioButton();
+		jRadioButton8.setText("Urban");
+		
+		jRadioButton9 = new JRadioButton();
+		jRadioButton9.setText("Yes");
+		
+		jRadioButton10 = new JRadioButton();
+		jRadioButton10.setText("No");
 	    
 	    //////////////////////////////////////////
+		
+		label1 = new JLabel();
+		label1.setText("Gender :");
+		
+		label2 = new JLabel();
+		label2.setText("Parents have own business :");
+		
+		label3 = new JLabel();
+		label3.setText("Part Time Job :");
+		
+		label4 = new JLabel();
+		label4.setText("Live Rural or Urban :");
+		
+		label5 = new JLabel();
+		label5.setText("Studies Business :");
+
+		
+		//////////////////////////////////////////
 	    
 	    textArea = new JTextArea();
 	    
 	    //////////////////////////////////////////
 	    
-	    frame.add(enterName);
-	    frame.add(enterSurname);
-	    frame.add(enterCity);
-	    frame.add(Save);
-	    frame.add(showAll);
-	    frame.add(delete);
+	    frame.add(label1);
+	    frame.add(jRadioButton1);
+	    frame.add(jRadioButton2);
+	    frame.add(label2);
+	    frame.add(jRadioButton3);
+	    frame.add(jRadioButton4);
+	    frame.add(label3);
+	    frame.add(jRadioButton5);
+	    frame.add(jRadioButton6);
+	    frame.add(label4);
+	    frame.add(jRadioButton7);
+	    frame.add(jRadioButton8);
+	    frame.add(label5);
+	    frame.add(jRadioButton9);
+	    frame.add(jRadioButton10);
+	    frame.add(Button1);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
-		if(e.getSource() == Save)
+		if(e.getSource() == Button1)
 		{
 			String name = enterName.getText();
 			String surname = enterSurname.getText();
@@ -100,7 +145,7 @@ public class naiveInterface  extends JFrame implements ActionListener{
 				enterCity.setText(null);
 			}
 		}
-		else if(e.getSource()== showAll)
+		else if(e.getSource()== Button2)
 		{
 			frame.add(textArea);
 			textArea.setColumns(6);
